b8b28cd34df3678e12e425d1422896170c3e9136
Eduardo Santos <33993373+edcsnt@users.noreply.github.com>
2025-06-25 06:30:28 -0300

    fix: read NULL(3const)
    on some obscure arches NULL may not be represented as all zeros I guess so avoid memset(3)

--- ht.h
+++ ht.h
@@ -24,75 +24,77 @@
 #define HT_OFFSET 2166136261u
 #define HT_PRIME    16777619u
 
-#define HT_HEAD(T, TK, TV)                                                    \
-        struct T##Entry {                                                     \
-                TK k;                                                         \
-                size_t n;                                                     \
-                TV v;                                                         \
-        };                                                                    \
-                                                                              \
-        struct T {                                                            \
-                struct T##Entry *entries;                                     \
-                size_t cap;                                                   \
-                size_t len;                                                   \
+#define HT_HEAD(T, TK, TV)                                              \
+        struct T##Entry {                                               \
+                TK k;                                                   \
+                size_t n;                                               \
+                TV v;                                                   \
+        };                                                              \
+                                                                        \
+        struct T {                                                      \
+                struct T##Entry *entries;                               \
+                size_t cap;                                             \
+                size_t len;                                             \
         };
 
-#define HT_INIT(ht)                                                           \
-        do {                                                                  \
-                memset((ht)->entries, 0, (ht)->cap * sizeof(*(ht)->entries)); \
-                (ht)->len = 0;                                                \
+#define HT_INIT(ht)                                                     \
+        do {                                                            \
+                size_t ht_i;                                            \
+                for (ht_i = 0; ht_i < (ht)->cap; ++ht_i)                \
+                        (ht)->entries[ht_i].k = NULL;                   \
+                (ht)->len = 0;                                          \
         } while (0)
 
-#define HT_INDEX(ht, k, n, i)                                                 \
-        do {                                                                  \
-                unsigned ht_h = HT_OFFSET;                                    \
-                const unsigned char *ht_b = (const unsigned char *)(k);       \
-                size_t ht_n = (n);                                            \
-                while (ht_n--)                                                \
-                        ht_h = (ht_h ^ (*ht_b++)) * HT_PRIME;                 \
-                (i) = ht_h & ((ht)->cap - 1);                                 \
+#define HT_INDEX(ht, k, n, i)                                           \
+        do {                                                            \
+                unsigned ht_h = HT_OFFSET;                              \
+                const unsigned char *ht_b = (const unsigned char *)(k); \
+                size_t ht_n = (n);                                      \
+                while (ht_n--)                                          \
+                        ht_h = (ht_h ^ (*ht_b++)) * HT_PRIME;           \
+                (i) = ht_h & ((ht)->cap - 1);                           \
         } while (0)
 
-#define HT_SET(ht, k, n, v)                                                   \
-        do {                                                                  \
-                size_t ht_i;                                                  \
-                HT_INDEX((ht), (k), (n), ht_i);                               \
+#define HT_SET(ht, k, n, v)                                             \
+        do {                                                            \
+                size_t ht_i;                                            \
+                HT_INDEX((ht), (k), (n), ht_i);                         \
                 while ((ht)->entries[ht_i].k &&
                      !((ht)->entries[ht_i].k == (k) &&
-                       (ht)->entries[ht_i].n == (n)))                         \
-                        ht_i = (ht_i + 1) & ((ht)->cap - 1);                  \
-                if (!(ht)->entries[ht_i].k)                                   \
-                        ++(ht)->len;                                          \
-                (ht)->entries[ht_i].k = (k);                                  \
-                (ht)->entries[ht_i].n = (n);                                  \
-                (ht)->entries[ht_i].v = (v);                                  \
+                       (ht)->entries[ht_i].n == (n)))                   \
+                        ht_i = (ht_i + 1) & ((ht)->cap - 1);            \
+                if (!(ht)->entries[ht_i].k)                             \
+                        ++(ht)->len;                                    \
+                (ht)->entries[ht_i].k = (k);                            \
+                (ht)->entries[ht_i].n = (n);                            \
+                (ht)->entries[ht_i].v = (v);                            \
         } while (0)
 
-#define HT_GET(ht, k, n, v)                                                   \
-        do {                                                                  \
-                size_t ht_i;                                                  \
-                (v) = NULL;                                                   \
-                HT_INDEX((ht), (k), (n), ht_i);                               \
-                while ((ht)->entries[ht_i].k) {                               \
-                        if ((ht)->entries[ht_i].k == (k) &&                   \
-                            (ht)->entries[ht_i].n == (n)) {                   \
-                                (v) = &((ht)->entries[ht_i].v);               \
-                                break;                                        \
-                        }                                                     \
-                        ht_i = (ht_i + 1) & ((ht)->cap - 1);                  \
-                }                                                             \
+#define HT_GET(ht, k, n, v)                                             \
+        do {                                                            \
+                size_t ht_i;                                            \
+                (v) = NULL;                                             \
+                HT_INDEX((ht), (k), (n), ht_i);                         \
+                while ((ht)->entries[ht_i].k) {                         \
+                        if ((ht)->entries[ht_i].k == (k) &&             \
+                            (ht)->entries[ht_i].n == (n)) {             \
+                                (v) = &((ht)->entries[ht_i].v);         \
+                                break;                                  \
+                        }                                               \
+                        ht_i = (ht_i + 1) & ((ht)->cap - 1);            \
+                }                                                       \
         } while (0)
 
-#define HT_GROW(dst, src)                                                     \
-        do {                                                                  \
-                size_t ht_i;                                                  \
-                HT_INIT(dst);                                                 \
-                for (ht_i = 0; ht_i < (src)->cap; ++ht_i)                     \
-                        if ((src)->entries[ht_i].k)                           \
-                                HT_SET((dst),                                 \
-                                       (src)->entries[ht_i].k,                \
-                                       (src)->entries[ht_i].n,                \
-                                       (src)->entries[ht_i].v);               \
+#define HT_GROW(dst, src)                                               \
+        do {                                                            \
+                size_t ht_i;                                            \
+                HT_INIT(dst);                                           \
+                for (ht_i = 0; ht_i < (src)->cap; ++ht_i)               \
+                        if ((src)->entries[ht_i].k)                     \
+                                HT_SET((dst),                           \
+                                       (src)->entries[ht_i].k,          \
+                                       (src)->entries[ht_i].n,          \
+                                       (src)->entries[ht_i].v);         \
         } while (0)
 
 #endif  /* HT_H */
